b35c95e4b20f52a077fedfedfc5be3a4d9fb37d6
Yoshikage Kira <spammly2.0@gmail.com>
2026-04-17 17:35:26 +1200

    Add MidnightLib dependency and update fabric.mod.json

--- src/main/java/com/h1ggsk/projcam/Projcam.java
+++ src/main/java/com/h1ggsk/projcam/Projcam.java
@@ -1,10 +1,13 @@
 package com.h1ggsk.projcam;
 
+import com.h1ggsk.projcam.config.ProjcamConfig;
+import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
 
 public class Projcam implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        MidnightConfig.init("projcam", ProjcamConfig.class);
     }
 }
